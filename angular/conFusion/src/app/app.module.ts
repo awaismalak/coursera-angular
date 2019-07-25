259e6d05975b38882b3401b28550de0854051c92
Muhammad Awais <awais.malik.q@gmail.com>
2019-07-25 20:40:36 +0500

    In this angular cours we cover angular matereal part 2

---
+++ angular/conFusion/src/app/app.module.ts
@@ -0,0 +1,42 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MatToolbarModule} from '@angular/material/toolbar';
+import { FlexLayoutModule} from '@angular/flex-layout';
+import { BrowserAnimationsModule } from'@angular/platform-browser/animations';
+import { MatListModule } from '@angular/material/list';
+import { MatCardModule, MatButtonModule } from '@angular/material';
+
+
+import { MatGridListModule } from '@angular/material/grid-list';
+
+
+
+
+import 'hammerjs';
+import { MenuComponent } from './menu/menu.component';
+import { from } from 'rxjs';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MenuComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MatToolbarModule,
+    FlexLayoutModule,
+    BrowserAnimationsModule,
+    MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatButtonModule
+ 
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
